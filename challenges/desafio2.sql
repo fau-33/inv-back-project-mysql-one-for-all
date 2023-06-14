2ac732810f85edc9c1de84d6c722c59020d9b53b
Flávio Félix <fau_felix30@hotmail.com>
2023-06-14 17:31:50 -0300

    Feito o desafio 2

--- challenges/desafio2.sql
+++ challenges/desafio2.sql
@@ -1,6 +1,8 @@
 SELECT
-    (SELECT COUNT(*) FROM cancoes) AS cancoes,
-    (SELECT COUNT(DISTINCT artista_id) FROM cancoes) AS artistas,
-    (SELECT COUNT(DISTINCT album_id) FROM cancoes) AS albuns;
+    COUNT(DISTINCT songs.song_id) AS cancoes,
+    COUNT(DISTINCT artists.artist_id) AS artistas,
+    COUNT(DISTINCT albums.album_id) AS albuns
 FROM
-    cancoes;
+    songs
+    INNER JOIN albums ON songs.album_id = albums.album_id
+    INNER JOIN artists ON albums.artist_id = artists.artist_id;
